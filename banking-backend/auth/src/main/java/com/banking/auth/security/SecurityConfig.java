5bb13494cb4ede7abb19002987ce94586e5afb2f
pooja02082020 <lohanipooja77@gmail.com>
2026-01-05 14:39:22 -0500

    changes in auth

--- banking-backend/auth/src/main/java/com/banking/auth/security/SecurityConfig.java
+++ banking-backend/auth/src/main/java/com/banking/auth/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.banking.auth.security;
 
-import com.banking.auth.security.JwtAuthenticationFilter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
