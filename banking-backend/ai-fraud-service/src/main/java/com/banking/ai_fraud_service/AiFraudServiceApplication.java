77ca5ed67aae056fd28b842c8f9ba315aa48e9ca
pooja02082020 <lohanipooja77@gmail.com>
2026-01-14 14:10:02 -0500

    added ai  fraud service

---
+++ banking-backend/ai-fraud-service/src/main/java/com/banking/ai_fraud_service/AiFraudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.ai_fraud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFraudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFraudServiceApplication.class, args);
+	}
+
+}
---
+++ banking-backend/ai-fraud-service/src/test/java/com/banking/ai_fraud_service/AiFraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.ai_fraud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
